e4b33bd096be06957e00b8c6becaa96a6060584e
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:16:27 +0700

    Add files via upload

---
+++ PBOIF2_10116378_LATIHAN15_Logika/src/pboif2_10116378_latihan15_logika/PBOIF2_10116378_LATIHAN15_Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116378_latihan15_logika;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class PBOIF2_10116378_LATIHAN15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
